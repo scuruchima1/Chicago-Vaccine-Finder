0f525950417da6bbf26977450eed100e04955b2b
scuruchima1 <64672490+scuruchima1@users.noreply.github.com>
2021-02-24 18:51:43 -0600

    Moved ran statement

--- vacdiscord.py
+++ vacdiscord.py
@@ -9,11 +9,12 @@ class VaccineNotification:
     def sendNotification(self,link):
         @self.client.event
         async def on_ready():
+            print('ran')
             await self.client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="vaccine appointments"))
             await self.client.guilds[0].channels[2].send("**Vaccine Found!**")
             await self.client.guilds[0].channels[2].send(link)
             await self.client.close()
-            print('ran')
+            
         self.client.run(config.discordbotapikey)
 
     def testNotification(self,link):
