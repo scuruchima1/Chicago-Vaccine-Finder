b5b8abae339f1018ae6431a5d7454a9f610ec58e
scuruchima1 <64672490+scuruchima1@users.noreply.github.com>
2021-02-24 20:28:41 -0600

    Fixed loop error
    Fixed loop error by adding sleep time for 0.1 second.

--- vacdiscord.py
+++ vacdiscord.py
@@ -1,5 +1,6 @@
 import discord
 import config
+import time
 
 class VaccineNotification:
     def __init__(self):
@@ -9,11 +10,12 @@ class VaccineNotification:
     def sendNotification(self,link):
         @self.client.event
         async def on_ready():
+            print('ran')
             await self.client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="vaccine appointments"))
             await self.client.guilds[0].channels[2].send("**Vaccine Found!**")
             await self.client.guilds[0].channels[2].send(link)
-            print('ran')
             await self.client.close()
+            time.sleep(0.1)
         self.client.run(config.discordbotapikey)
 
     def testNotification(self,link):
@@ -24,4 +26,5 @@ class VaccineNotification:
             await self.client.guilds[0].channels[2].send(link)
             print('ran test notification')
             await self.client.close()
+            time.sleep(0.1)
         self.client.run(config.discordbotapikey)
